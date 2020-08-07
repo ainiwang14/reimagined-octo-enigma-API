1bdb11fd89449c086bfe6b7ea61cf14a01b8461b
Oliver Bigelow <oelucy@gmail.com>
2020-08-06 18:30:26 -0700

    now able to select state and national park info appends to page

---
+++ startbootstrap-grayscale-gh-pages/js/results.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+    $(".dropdown-item").on("click", function(){
+        $(".results").empty()
+
+    var stateCode = $(this).attr("value")
+    console.log(stateCode)
+    var queryUrl = "https://developer.nps.gov/api/v1/parks?stateCode=" + stateCode + "&api_key=8TwvOB64CVZ7My6tRYYfqqq4Tz82BObwHj5wzbyX"
+
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function(response){
+        var lat0 = response.data[0].latitude
+        var long0 = response.data[0].longitude
+        var lat1 = response.data[1].latitude
+        var long1 = response.data[1].longitude
+        var lat2 = response.data[2].latitude
+        var long2 = response.data[2].longitude
+        var lat3 = response.data[3].latitude
+        var long3 = response.data[3].longitude
+        var lat4 = response.data[4].latitude
+        var long4 = response.data[4].longitude
+        var lat5 = response.data[5].latitude
+        var long5 = response.data[5].longitude
+        console.log(response)
+        for (var i = 0; i < 5; i++){
+            var resultDiv = $("<div>")
+            var parkName = $("<p>")
+            var info = $("<p>")
+            var fees = $("<p>")
+            if (response.data[i].entranceFees[i]) {
+                fees.text(response.data[i].entranceFees[i].cost)
+            }
+            else {
+                fees.text("") 
+            }
+            parkName.text(response.data[i].fullName)
+            info.text(response.data[i].description)
+            resultDiv.append(parkName, info, fees)
+            $(".results").append(resultDiv)
+            console.log(response.data[i].fullName)
+        }
+    })
+  }) 
+})
