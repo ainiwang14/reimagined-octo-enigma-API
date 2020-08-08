acf43f73cb6c28f9004087c2887644ec15a9daaf
AileenWang <ainiwang14@gmail.com>
2020-08-07 19:17:03 -0700

    omg the zipcode API works ty Collin Crom

--- startbootstrap-grayscale-gh-pages/js/results.js
+++ startbootstrap-grayscale-gh-pages/js/results.js
@@ -1,7 +1,4 @@
 $(document).ready(function(){
-    $(document).on("click", ".infoBtn", function(){
-        console.log("hello")
-    })
     var stateArray = [{ initials: 'AL', state: 'Alabama' }, { initials: 'AZ', state: 'Arizona' }, {initials: 'AK', state: 'Alaska'}, {initials: 'AZ', state: 'Arizona'}, {initials: 'AR', state: 'Arkansas'}, {initials: 'CA', state: 'California'}, {initials: 'CO', state: 'Colorado'}, {initials: 'CT', state: 'Connecticut'}, {initials: 'DE', state: 'Delaware'}, {initials: 'FL', state: 'Florida'}, {initials: 'GA', state: 'Georgia'}, {initials: 'HI', state: 'Hawaii'}, {initials: 'ID', state: 'Idaho'}, {initials: 'IL', state: 'Illinois'}, {initials: 'IN', state: 'Indiana'}, {initials: 'IA', state: 'Iowa'}, {initials: 'KS', state: 'Kansas'}, {initials: 'KY', state: 'Kentucky'}, {initials: 'LA', state: 'Louisiana'}, {initials: 'ME', state: 'Maine'}, {initials: 'MD', state: 'Maryland'}, {initials: 'MA', state: 'Massachusetts'}, {initials: 'ME', state: 'Maine'}, {intials: 'MI', state: 'Michigan'}, {initials: 'MN', state: 'Minnesota'}, {initials: 'MS', state: 'Mississippi'}, {initials: 'MO', state: 'Missouri'}, {intials: 'MT', state: 'Montana'}, {initials: 'NE', state: 'Nebraska'}, {initials: 'NV', state: 'Nevada'}, {initals: 'NH', state: 'New Hampshire'}, {initals: 'NJ', state: 'New Jersey'}, {initials: 'NM', state: 'New Mexico'}, {initials: 'NY', state: 'New York'}, {initials: 'NC', state: 'North Carolina'}, {initials: 'ND', state: 'North Dakota'}, {initials: 'OH', state: 'Ohio'}, {initials: 'OK', state: 'Oklahoma'}, {initials: 'OR', state: 'Oregon'}, {initials: 'PA', state: 'Pennsylvania'}, {initials: 'RI', state: 'Rhode Island'}, {initials: 'SC', state: 'South Carolina'}, {initials: 'SD', state: 'South Dakota'}, {initials: 'TN', state: 'Tennessee'}, {initials: 'TX', state: 'Texas'}, {initials: 'UT', state: 'Utah'}, {initials: 'VT', state: 'Vermont'}, {initials: 'VA', state: 'Virginia'}, {initials: 'WA', state: 'Washington'}, {initials: 'WV', state: 'West Virginia'}, {initials: 'WI', state: 'Wisconsin'}, {initials: 'WY', state: 'Wyoming'}];
     for (var i = 0; i < stateArray.length; i++) {
         var option = $("<option>");
@@ -28,10 +25,10 @@ $(document).ready(function(){
         method: "GET"
     }).then(function(response){
         var map;
-        map = new google.maps.Map(document.getElementById("map"), {
-            center: new google.maps.LatLng(0,0),
-            zoom: 5
-        })
+        // map = new google.maps.Map(document.getElementById("map"), {
+        //     center: new google.maps.LatLng(0,0),
+        //     zoom: 5
+        // })
         for (var i = 0; i < 3; i++) {
 
         var name = response.data[i].fullName
@@ -44,31 +41,31 @@ $(document).ready(function(){
             url: geocodingUrl,
             method: "GET"
         }).then(function(addResponse){
-            function initMap() {
-                map.panTo(new google.maps.LatLng(addResponse.results[0].geometry.location.lat,addResponse.results[0].geometry.location.lng))
-                getParks();
-                }
-                function getParks() {
-                    console.log(name + "2")
-                        var marker = new google.maps.Marker({
-                            position: addResponse.results[0].geometry.location,
-                            map: map,
-                            title: addResponse.results[0].name
-                        });
-                        var infoWindow = new google.maps.InfoWindow({
-                            content: content
-                        })
-                        marker.setMap(map);
-                        bindInfoWindow(marker, map, infoWindow, content);
-                }
+            // function initMap() {
+            //     map.panTo(new google.maps.LatLng(addResponse.results[0].geometry.location.lat,addResponse.results[0].geometry.location.lng))
+            //     getParks();
+            //     }
+            //     function getParks() {
+            //         console.log(name + "2")
+            //             var marker = new google.maps.Marker({
+            //                 position: addResponse.results[0].geometry.location,
+            //                 map: map,
+            //                 title: addResponse.results[0].name
+            //             });
+            //             var infoWindow = new google.maps.InfoWindow({
+            //                 content: content
+            //             })
+            //             marker.setMap(map);
+            //             bindInfoWindow(marker, map, infoWindow, content);
+            //     }
 
-                function bindInfoWindow(marker, map, infoWindow, html) {
-                    marker.addListener("click", function(){
-                        infoWindow.setContent(html)
-                        infoWindow.open(map, this)
-                    })
-                }
-            initMap()
+            //     function bindInfoWindow(marker, map, infoWindow, html) {
+            //         marker.addListener("click", function(){
+            //             infoWindow.setContent(html)
+            //             infoWindow.open(map, this)
+            //         })
+            //     }
+            // initMap()
         })
     }
         console.log(response)
@@ -81,8 +78,9 @@ $(document).ready(function(){
             var resultBtn = $("<button>")
             resultBtn.text("More Info")
             resultBtn.attr("class", "infoBtn")
-            resultBtn.click(() => handleInfoClick(zipcode))
-            const zipcode = response.data[i].addresses[0].postalcode
+            const zipcode = response.data[i].addresses[0].postalCode
+            resultBtn.attr("id", `${zipcode}`)
+            resultBtn.click((event) => handleInfoClick(event.currentTarget.id))
             if (response.data[i].images[0]) {
                 parkImg.attr("src", response.data[i].images[0].url)
                 parkImg.attr("alt", response.data[i].images[0].altText)
@@ -110,7 +108,6 @@ function zipToGeo(zipcode) {
     }).then(response => {
         const latitude = response.records[0].fields.latitude;
         const longitude = response.records[0].fields.longitude;
-        console.log(latitude, longitude)
         return {latitude, longitude}
     })
 }
