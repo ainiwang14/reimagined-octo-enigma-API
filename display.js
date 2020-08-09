330f4b931f1b558e58665e060f2b8128f6f857cc
Jesus DeLintt <jesus.delintt@gmail.com>
2020-08-09 01:05:54 -0700

    added to display.js

--- display.js
+++ display.js
@@ -2,5 +2,5 @@ $(document).ready(function () {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const zipCode = urlParams.get("PostalCode");
-  console.log(PostalCode);
+  alert(zipCode);
 });
