9ab256bc4354a54b101923c13158bb9daa81e21b
Jesus DeLintt <jesus.delintt@gmail.com>
2020-08-09 00:42:39 -0700

    created display.js file

---
+++ display.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const zipCode = urlParams.get("PostalCode");
+  console.log(PostalCode);
+});
