02e1046c8bf7dead21ba4829a3092516101df629
Jesus DeLintt <jesus.delintt@gmail.com>
2020-08-10 12:50:07 -0700

    added code for ajax calls with weather and converted it to fahrenheit

--- js/display.js
+++ js/display.js
@@ -10,6 +10,25 @@ $(document).ready(function () {
     url: latLongUrl,
     method: "GET",
   }).then((response) => {
-    console.log(response);
+    var longitude = response.records[0].fields.longitude;
+    var latitude = response.records[0].fields.latitude;
+    var weatherUrl =
+      "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+      latitude +
+      "&lon=" +
+      longitude +
+      "&appid=927924e4c73455c7286d71a6b1b45a4c";
+    $.ajax({
+      url: weatherUrl,
+      method: "GET",
+    }).then((response) => {
+      console.log(response);
+      console.log(response.current.temp);
+      var currentTemp = response.current.temp;
+      currentTemp = currentTemp - 273;
+      currentTemp = currentTemp * (9 / 5);
+      currentTemp = Math.floor(currentTemp + 32);
+      console.log(currentTemp);
+    });
   });
 });
