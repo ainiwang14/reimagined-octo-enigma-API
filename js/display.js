6539132913c585346f1651a7ea7f6ce3dff5a096
Jesus DeLintt <jesus.delintt@gmail.com>
2020-08-10 14:56:49 -0700

    put in our sun icon!

--- js/display.js
+++ js/display.js
@@ -34,6 +34,12 @@ $(document).ready(function () {
       currentTemp = currentTemp * (9 / 5);
       currentTemp = Math.floor(currentTemp + 32);
       console.log(currentTemp);
+      $("#weatherbox").html(currentTemp);
+      // if (currentTemp < 65) {
+      var image = $("<img>");
+      image.attr("src", "assets/img/sun-icon.png");
+      $("#weatherbox").append(image);
+      // }
     });
   });
 });
